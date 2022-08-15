b51cd2efbfdb59f80cac9de0393c839616a924dd
thinker227 <thinker0227@gmail.com>
2022-08-15 18:58:36 +0200

    Fix bit order

--- src/BitMagic.hs
+++ src/BitMagic.hs
@@ -13,10 +13,10 @@ boolToBit :: Bool -> Int
 boolToBit b = if b then 1 else 0
 
 getNybbleBits num =
-    [ bitIs1 0 num,
-      bitIs1 1 num,
+    [ bitIs1 3 num,
       bitIs1 2 num,
-      bitIs1 3 num ]
+      bitIs1 1 num,
+      bitIs1 0 num ]
 
 getIntFromBits' :: [Bool] -> Int -> Int
 getIntFromBits' [bit] num = num .|. boolToBit bit
